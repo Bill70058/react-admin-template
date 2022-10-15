bb15cae725d2052664599465bc999a04a7922d46
lzr <lzr@email.com>
2022-10-15 19:35:58 +0800

    init v2

--- src/pages/Home/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react'
-function Home() {
-  useEffect(() => {
-    // console.log(JSON.parse(sessionStorage.getItem('userInfo') || ''))
-  })
-  return <div>Home</div>
-}
-
-export default Home
